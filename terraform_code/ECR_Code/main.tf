eae01e2d7a01a40e42a9fc2e0bbc32fc78aa4e3d
varnika <varnika.varnika.com>
2026-02-08 23:57:33 +0000

    Added .gitignore

---
+++ terraform_code/ECR_Code/main.tf
@@ -0,0 +1,65 @@
+
+resource "aws_ecr_repository" "cats-repo" {
+  name = "project1-cats"
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+}
+
+resource "aws_ecr_repository" "dogs-repo" {
+  name = "project1-dogs"
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+}
+resource "aws_ecr_repository_policy" "repo-policy-cats" {
+  repository = aws_ecr_repository.cats-repo.name
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "adds full ecr access to the demo repository",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_ecr_repository_policy" "repo-policy-dogs" {
+  repository = aws_ecr_repository.dogs-repo.name
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "adds full ecr access to the demo repository",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF
+}
